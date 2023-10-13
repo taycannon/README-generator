4fb74590ec0051d17f19ee020408a0e1332068e5
taycannon <taycannon2003@gmail.com>
2023-10-13 15:58:32 -0500

    Only have the question pop up once but createREADME not defined

--- Develop/index.js
+++ Develop/index.js
@@ -4,71 +4,75 @@ const fs = require('fs')
 
 // TODO: Create an array of questions for user input
 const questions = [
-{
+    {
         type: 'input',
         message: 'What is the title of your project?',
         name: 'Title',
-        },
-        {
+    },
+    {
         type: 'input',
         message: 'What is the description of your project?',
         name: 'Description',
-        },
-        
-        
-        {
+    },
+
+
+    {
         type: 'input',
         message: 'Does users need to install anything?',
         name: 'Installation',
-        },
-        
-        
-        {
+    },
+
+
+    {
         type: 'input',
         message: 'What is the use for this project?',
         name: 'Usage',
-        },
-        
-        
-        {
+    },
+
+
+    {
         type: 'input',
         message: 'Did you use outside help or work with others? If so put them here.',
         name: 'Credits',
-        },
-        
-        
-        {
+    },
+
+
+    {
         type: 'list',
         message: 'Did you have a license for this project? If so pick one!',
         name: 'License',
-        choices: ['Academic Free License v3.0', 'Apache license 2.0', 'Boost Software License 1.0', 
-        'Microsoft Public License', 'MIT','GNU Affero General Public License v3.0', 'GNU General Public License family', 'N/A']
+        choices: ['Academic Free License v3.0', 'Apache license 2.0', 'Boost Software License 1.0',
+            'Microsoft Public License', 'MIT', 'GNU Affero General Public License v3.0', 'GNU General Public License family', 'N/A']
 
     }
 
- ];
-    
- inquirer.prompt(questions) 
- .then((answers) => {
-    console.log(answers);
-    let document = `
- 
- # ${answers.Title}
+];
 
-   ${answers.Description}
+function init() {
+ inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+const document = `
 
-   ${answers.Installation}
+# ${answers.Title}
 
-   ${answers.Usage}
+${answers.Description}
 
-   ${answers.Credits}
+${answers.Installation}
 
-   ${answers.License}
+${answers.Usage}
 
- `
+${answers.Credits}
 
-writeToFile(document)
+${answers.License}
+`
+
+writeToFile(document);
 })
+    .catch((error) => {
+    console.error('Error occurred:', error);
+    });
+}
 
 
 // TODO: Create a function to write README file
